2fa366bb375bb5decf26ef16a21a50d996242d66
Basti <bastian.romer@web.de>
2023-03-31 08:50:52 +0200

    welcome text

--- src/main/scala/ConnectFour.scala
+++ src/main/scala/ConnectFour.scala
@@ -5,5 +5,7 @@ object ConnectFour:
 
   def main(args: Array[String]): Unit =
     val field = new Field
-    println("\u001B[31mWelcome to connect four!!\u001B[0m")
+    println("\u001B[32mWelcome to connect four!!")
+    println("\u001B[31mPlayer1")
+    println("\u001B[33mPlayer2\u001B[0m")
     print(field.printField())
