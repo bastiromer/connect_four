8a165b6c9a83880b5f3a9093a244a6cb2343e05e
Basti <bastian.romer@web.de>
2023-04-21 09:52:46 +0200

    tui first approach

--- src/main/scala/ConnectFour.scala
+++ src/main/scala/ConnectFour.scala
@@ -1,8 +1,35 @@
 import model._
 
+import scala.io.StdIn.readLine
+
+
 object ConnectFour:
 
-  def main(args: Array[String]): Unit =
-    var field = new Field(size = 3, filling = Stone.Empty)
+
+  @main def run: Unit =
     println("\u001B[31mWelcome to connect four!!\u001B[0m")
-    print(field.toString)
+    val field = new Field(7, 6, Stone.Empty)
+    println(field.toString)
+    getInputAndPrintLoop(field)
+
+  def getInputAndPrintLoop(field: Field): Unit =
+    println("Enter yout move <player><row>")
+    val input = readLine
+    parseInput(input, field) match
+      case None => field
+      case Some(newfield) =>
+        println(newfield)
+        getInputAndPrintLoop(newfield)
+
+  def parseInput(input: String, field: Field): Option[Field] =
+    input match
+      case "q" => None
+      case _ => {
+        val chars = input.toCharArray
+        val stone = chars(0) match
+          case '1' => Stone.X
+          case '2' => Stone.O
+          case _ => Stone.Empty
+        val x = chars(1).toString.toInt
+        Some(field.put(stone, x))
+      }
