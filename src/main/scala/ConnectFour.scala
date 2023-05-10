835866a8d378f4bd62a9cf34d33891deb9a28dfa
bastiromer <116097106+bastiromer@users.noreply.github.com>
2023-05-10 23:33:52 +0200

    Update ConnectFour.scala

--- src/main/scala/ConnectFour.scala
+++ src/main/scala/ConnectFour.scala
@@ -3,6 +3,5 @@ import model._
 object ConnectFour:
 
   def main(args: Array[String]): Unit =
-    var field = new Field(size = 3, filling = Stone.Empty)
     println("\u001B[31mWelcome to connect four!!\u001B[0m")
     print(field.toString)
