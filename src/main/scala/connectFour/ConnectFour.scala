2e91e4bdc1202f72b56bbf9033fbd3ca9c0852af
Basti <bastian.romer@web.de>
2023-05-10 23:25:39 +0200

    field size change

--- src/main/scala/connectFour/ConnectFour.scala
+++ src/main/scala/connectFour/ConnectFour.scala
@@ -7,7 +7,7 @@ import controller.Controller
 object ConnectFour:
   @main def run: Unit =
     println("\u001B[31mWelcome to connect four!!\u001B[0m")
-    val field = new Field(3, 2, Stone.Empty)
+    val field = new Field(7, 6, Stone.Empty)
     val controller = Controller(field)
     val tui = TUI(controller)
     tui.run
