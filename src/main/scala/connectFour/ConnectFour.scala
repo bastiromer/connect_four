2fe2ec3dacc9ff10216ec97e17a92acc68ef0a36
montaser717 <montasern301@gmail.com>
2023-05-26 11:59:37 +0200

    further development TUI

--- src/main/scala/connectFour/ConnectFour.scala
+++ src/main/scala/connectFour/ConnectFour.scala
@@ -1,13 +1,13 @@
 package connectFour
 
-import aview.TUI
+import connectFour.aview.TUI
 import model.{Field, Stone}
-import controller.Controller
+import controller.{Controller, TUIMoveValidator}
 
 object ConnectFour:
   @main def run: Unit =
     println("\u001B[31mWelcome to connect four!!\u001B[0m")
     val field = new Field(7, 6, Stone.Empty)
-    val controller = Controller(field)
+    val controller = Controller(field,TUIMoveValidator)
     val tui = TUI(controller)
     tui.run
