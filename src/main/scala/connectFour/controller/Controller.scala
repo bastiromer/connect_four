df9826806d817386256e0896d0a00eafdec5594d
Basti <bastian.romer@web.de>
2023-06-15 15:59:48 +0200

    player Logig and doAndPublish method updated

--- src/main/scala/connectFour/controller/Controller.scala
+++ src/main/scala/connectFour/controller/Controller.scala
@@ -2,28 +2,43 @@ package connectFour
 package controller
 
 import model.{Field, Move, Player, PlayerFactory, Stone}
-import util.{Command, Observable, UndoManager}
+import util.{Command, Event, Observable, UndoManager}
 
-case class Controller(var field: Field, moveValidator: MoveValidator) extends Observable:
-  var gameState: GameState = InProgressState
+case class Controller(var field: Field) extends Observable:
   val undoManager = new UndoManager[Field]
-  def changeGameState(state: GameState): Unit =
-    gameState = state
+
   
   def doAndPublish(doThis: Move => Field, move: Move) =
     field = doThis(move)
-    notifyObservers
+    field.checkWin() match
+      case Some(Stone.X) | Some(Stone.O) => notifyObservers(Event.End)
+      case None => changePlayer;notifyObservers(Event.Move)
+
   def doAndPublish(doThis: => Field) =
     field = doThis
-    notifyObservers
+    field.checkWin() match
+      case Some(Stone.X) | Some(Stone.O) => notifyObservers(Event.End)
+      case None => changePlayer;notifyObservers(Event.Move)
   def put(move: Move): Field = undoManager.doStep(field, PutCommand(move))
   def undo: Field = undoManager.undoStep(field)
   def redo: Field = undoManager.redoStep(field)
-
-  def restartGame(): Unit =
-    gameState.restartGame(this)
-    notifyObservers
-
-  override def toString: String = gameState.printfField(field).get
+  
+  override def toString: String = field.toString
   
   def getCol(row: Int): Int = field.moveCorrect(row)
+  
+  def checkWin(): Option[Player] =
+    field.checkWin().get match
+      case p1.stone => Some(p1)
+      case p2.stone => Some(p2)
+      case _ => None
+
+  def changePlayer: Unit =
+    if currentPlayer == p1
+    then currentPlayer = p2
+    else currentPlayer = p1
+
+  val p1: Player = new PlayerFactory().createPlayer("Player1", "red")
+  val p2: Player = new PlayerFactory().createPlayer("Player2", "yellow")
+  var currentPlayer: Player = p1
+
