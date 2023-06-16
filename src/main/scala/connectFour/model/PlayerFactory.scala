e3b92b050fec77d2002dc28b37c6740f58f9567f
montaser717 <montasern301@gmail.com>
2023-06-16 11:17:22 +0200

    Controller Interface

--- src/main/scala/connectFour/model/PlayerFactory.scala
+++
@@ -1,9 +0,0 @@
-package connectFour.model
-
-class PlayerFactory:
-  def createPlayer(name: String, color: String): Player =
-    color match
-      case "red" => new HumanPlayer(Stone.X, name)
-      case "yellow" => new HumanPlayer(Stone.O, name)
-      case "start" => new HumanPlayer(Stone.Empty, name)
-      case _ => throw new IllegalArgumentException("Ungültige Spieler eingabe!")
